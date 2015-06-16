273e9f2d81633e0fb18d180474e9ef021f1fe9b6
Meg Svetz <megsvetz@gmail.com>
2015-06-16 11:53:08 -0600

    added new headers hoping to change innerhtml

--- app/assets/javascripts/personal-budget.js
+++ app/assets/javascripts/personal-budget.js
@@ -3,5 +3,10 @@ var moneyButton = document.getElementById('money_made_button');
 var totalMoney = 0;
 moneyButton.addEventListener('click',function(){
   totalMoney += Number(moneyInput.value);
-  console.log(totalMoney);
+  document.getElementById("money_coming_in").innerhtml(totalMoney);
 });
+
+
+
+document.getElementById("money_going_out").innerhtml(totalBills);
+document.getElementById("money_left_over").innerhtml(totalMoneyMinusTotalBills);
